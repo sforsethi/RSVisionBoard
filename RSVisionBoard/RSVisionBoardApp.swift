c2d799b634dcc4d9d607b37a808600cac879e532
raghav-latte <131696988+raghav-latte@users.noreply.github.com>
2025-09-29 20:26:51 +0530

    Initial Commit

---
+++ RSVisionBoard/RSVisionBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSVisionBoardApp.swift
+//  RSVisionBoard
+//
+//  Created by Raghav Sethi on 29/09/25.
+//
+
+import SwiftUI
+
+@main
+struct RSVisionBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
